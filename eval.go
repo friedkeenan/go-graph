4f94ebce4065dacbb5c9f1332d3d5e72f897a9dd
friedkeenan <friedkeenan@protonmail.com>
2020-06-03 07:26:05 -0500

    More functions

--- eval.go
+++ eval.go
@@ -9,20 +9,60 @@ import (
 
 var (
     Functions = map[string]govaluate.ExpressionFunction {
-        "sin": func (args ...interface{}) (interface{}, error) {
-            return math.Sin(args[0].(float64)), nil
+        "abs": func (args ...interface{}) (interface{}, error) {
+            return math.Abs(args[0].(float64)), nil
+        },
+
+        "acos": func (args ...interface{}) (interface{}, error) {
+            return math.Acos(args[0].(float64)), nil
+        },
+
+        "acosh": func (args ...interface{}) (interface{}, error) {
+            return math.Acosh(args[0].(float64)), nil
+        },
+
+        "asin": func (args ...interface{}) (interface{}, error) {
+            return math.Asin(args[0].(float64)), nil
+        },
+
+        "asinh": func (args ...interface{}) (interface{}, error) {
+            return math.Asinh(args[0].(float64)), nil
+        },
+
+        "atan": func (args ...interface{}) (interface{}, error) {
+            return math.Atan(args[0].(float64)), nil
+        },
+
+        "atan2": func (args ...interface{}) (interface{}, error) {
+            return math.Atan2(args[0].(float64), args[1].(float64)), nil
+        },
+
+        "atanh": func (args ...interface{}) (interface{}, error) {
+            return math.Atanh(args[0].(float64)), nil
+        },
+
+        "ceil": func (args ...interface{}) (interface{}, error) {
+            return math.Ceil(args[0].(float64)), nil
         },
 
         "cos": func (args ...interface{}) (interface{}, error) {
             return math.Cos(args[0].(float64)), nil
         },
 
-        "tan": func (args ...interface{}) (interface{}, error) {
-            return math.Tan(args[0].(float64)), nil
+        "cosh": func (args ...interface{}) (interface{}, error) {
+            return math.Cosh(args[0].(float64)), nil
         },
 
         "exp": func (args ...interface{}) (interface{}, error) {
             return math.Exp(args[0].(float64)), nil
+        },
+
+        "floor": func (args ...interface{}) (interface{}, error) {
+            return math.Floor(args[0].(float64)), nil
+        },
+
+        "gamma": func (args ...interface{}) (interface{}, error) {
+            return math.Gamma(args[0].(float64)), nil
         },
 
         "ln": func (args ...interface{}) (interface{}, error) {
@@ -32,6 +72,27 @@ var (
         "log": func (args ...interface{}) (interface{}, error) {
             return math.Log10(args[0].(float64)), nil
         },
+
+        "sin": func (args ...interface{}) (interface{}, error) {
+            return math.Sin(args[0].(float64)), nil
+        },
+
+        "sinh": func (args ...interface{}) (interface{}, error) {
+            return math.Sinh(args[0].(float64)), nil
+        },
+
+        "sqrt": func (args ...interface{}) (interface{}, error) {
+            return math.Sqrt(args[0].(float64)), nil
+        },
+
+        "tan": func (args ...interface{}) (interface{}, error) {
+            return math.Tan(args[0].(float64)), nil
+        },
+
+        "tanh": func (args ...interface{}) (interface{}, error) {
+            return math.Tanh(args[0].(float64)), nil
+        },
+
     }
 
     Constants = map[string]interface{} {
